802c92160c305d5358bffd29de4aac2e91ce75cf
SergeyPopenko <skino4ek@ukr.net>
2018-06-08 17:56:27 +0300

    module 3

---
+++ js/setup.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+(function () {
+  var setup = document.querySelector('.setup');
+  setup.classList.remove('hidden');
+})();
+
+(function () {
+  var arrPlayers = [];
+  console.log("arr created");
+})();
